69dc0222af68f082c329444127e4166130732f6a
m1st1xx <sashabalandin14@gmail.com>
2026-01-31 16:05:27 +0300

    first commit

--- pycharmproject/restring.py
+++
@@ -1,5 +0,0 @@
-def is_palindrome(text):
-    processed_text = "".join(text.split()).lower()
-    return processed_text == processed_text[::-1]
-
-print(is_palindrome("потоп"))
--- pycharmproject/reverse.py
+++
@@ -1,7 +0,0 @@
-mn={"a": 1, "b": 2}
-def reverse_dict(mn):
-    reversed_mn = {}
-    for key, value in mn.items():
-        reversed_mn[value] = key
-    print(reversed_mn)
-reverse_dict(mn)
